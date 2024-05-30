71590c9cac1a48309030de7bf1ede234d76de758
TayyibDawood <tayyibdawood13@gmail.com>
2024-05-30 16:57:07 +0200

    <added ingredient class (implemenetd to represent properties such as name, quantity, unit, calories, food group)>

--- Ingredient.cs
+++ Ingredient.cs
@@ -8,7 +8,7 @@ namespace PROG6221POE
 {
     public class Ingredient
     {
-        public string Name { get; set; } // Defining a property called Name
+        public string Name { get; set; } // Defining a property called Name.
         public double Quantity { get; set; } // Defining a property called Quantity
         public string Unit { get; set; } //Defining a property called Unit
         public double Calories { get; set; } // Defining a property called Calories
